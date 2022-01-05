1df055a060072eac60d462ea2549b4c330191bb9
natifdallegrave <96789492+natifdallegrave@users.noreply.github.com>
2022-01-04 23:38:18 -0300

    comit final

--- README.md
+++ README.md
@@ -10,9 +10,9 @@ Introdução
 
 Recursos
  
-Possui 3 páginas, uma para a apresentação do que podemos oferecer, outra de execução, onde puxamos os dados que queremos da api e outra de cadastro.
+Possui 4 páginas, uma para a apresentação do que podemos oferecer, outra de execução, onde puxamos os dados que queremos da api de forma aleatória, uma onde o usuário digita a raça a ser mostrada na tela e outra de cadastro.
 
 Processo de construção
-Gerar os três arquivos HTML e CSS;
+Gerar os quatro arquivos HTML e CSS;
 Puxar a Api através do FETCH no js ;
 Validar o E-mail com regex;
--- main.js
+++ main.js
@@ -11,15 +11,15 @@ fetch(url)
        throw new Error("error") 
     }
 }).then((data) => {dogs = data.message;
-    for(let dog in dogs){
-        const imageDiv = document.getElementById("image");
-        const dogImg = document.createElement("img");
-        dogImg.className = "img";
-        dogImg.src= dogs[dog];
+    // for(let dog in dogs){
+        const imageDiv = document.getElementById("img1");
+        // const dogImg = document.createElement("img");
+        imageDiv.className = "img";
+        imageDiv.src= dogs;
         console.log(dogImg);
-        imageDiv.appendChild(dogImg);
+        // imageDiv.appendChild(dogImg);
         // document.body.style.backgroundImage = `url('${dogs}')`
-    }
+    // }
 console.log(dogs)} )
 .catch((error)=>
 console.log(error)
@@ -32,4 +32,38 @@ console.log(error)
 }
 
 
+function procura(){
+    var variavel = document.getElementById("achei").value;
+    const url = `https://dog.ceo/api/breed/${variavel}/images/random`
+    console.log(url)
+    var dogs = new Array();
+    fetch(url)
+    .then((res) => {
+        if(res.ok){
+            return res.json()
+             
+    
+        }
+        else{
+           throw new Error("error") 
+        }
+      
+    }).then((data) => {dogs = data.message;
+        const imageDiv = document.getElementById("img");
+        imageDiv.className = "img";
+        imageDiv.src= dogs;
+        console.log(dogImg);
+        // imageDiv.appendChild(dogImg);
+        // document.body.style.backgroundImage = `url('${dogs}')`
+    
+    console.log(dogs)} )
+    .catch((error)=>
+    console.log(error)
+    )
+        
+        // dogImage
+        
+    
+        
+    }
         
